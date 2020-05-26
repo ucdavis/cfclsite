b4dc9e2231859f7e92ef203bd9cc178887a8a39f
Daniel Ritchie <dritchie1031@gmail.com>
2020-05-26 12:48:32 -0700

    many updates

--- storyalert.js
+++
@@ -1,8 +0,0 @@
-/*
-function alerter() {
-    alert("Stories spreadsheet coming soon!");
-    console.log("Alerter called")
-}
-let elem = document.getElementById('nostory');
-elem.addEventListener('click', alerter);
-*/
